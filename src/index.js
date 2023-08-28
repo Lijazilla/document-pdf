22c63738e36d7bba431b364f105fb39ec14c3598
Lijazilla <alejandro-1986-jf@hotmail.com>
2023-08-28 08:53:09 -0600

    se modificó el index

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 // import { BrowserRouter as Router } from "react-router-dom";
 // import MainRouter from "./mainRouter"; 
-import reportWebVitals from './reportWebVitals';
+
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
@@ -12,4 +12,3 @@ root.render(
   
 );
 
-reportWebVitals();
